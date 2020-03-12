2057c3067c0c75187466b70b6cdf3bc8f2dc8c7b
ramoncarrascom <ramon@ramoncarrasco.es>
2020-03-12 19:18:46 +0100

    ADDED data project

---
+++ RecipesAppNetCoreData/contexts/RecipesContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecipesAppNetCoreData.domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecipesAppNetCoreData.contexts
+{
+    /// <summary>
+    /// Recipes context configuration for Entity Framework
+    /// </summary>
+    public class RecipesContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<Recipe> Recipes { get; set; }
+        public DbSet<UnitOfMeasure> Uoms { get; set; }
+
+        /// <summary>
+        /// Context configuration
+        /// </summary>
+        /// <param name="optionsBuilder">Context options builder</param>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySQL(@"server=192.168.100.101;database=recipes_net;user=recipesuser;password=1234");
+            }            
+        }
+    }
+}
